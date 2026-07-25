8fd0d56eef3f9b8c3dcb6d8887c61d624e0e814c
unknown <advoluteedge@gmail.com>
2026-07-25 17:39:29 +0800

    Update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -188,7 +188,7 @@ export default function Home() {
           <div className="portrait-dots" aria-hidden="true" />
           <div className="portrait-card">
             <img
-              src="/assets/ma-nikka-noble.jpg"
+              src="/asset/nikka-noble.jpg"
               alt="Professional portrait of Ma. Nikka Noble"
             />
           </div>
